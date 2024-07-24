8bcf460f998c412341d8a318df32052f09342227
takipone <takipone@gmail.com>
2024-07-24 11:00:13 +0900

    画像ファイルの追加と商品データの更新

--- app/components/news.tsx
+++ app/components/news.tsx
@@ -1,5 +1,6 @@
-import Link from 'next/link'
+'use client'
 
+import Link from 'next/link'
 import newsData from '../../devcamp_news.json';
 
 interface News {
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export'
+  output: 'export',
+  trailingSlash: true,
+  images: { unoptimized: true }
 };
 
 export default nextConfig;
